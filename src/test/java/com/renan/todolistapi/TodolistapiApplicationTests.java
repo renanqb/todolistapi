d85d29741d90af91bd74df840f0235394d468ef4
Renan Viana Marcelino <renan.marcelino@gmail.com>
2021-09-17 15:56:00 -0300

    chore: initial springboot web project setup

---
+++ src/test/java/com/renan/todolistapi/TodolistapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.todolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
